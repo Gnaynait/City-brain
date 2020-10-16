37e16e3b4271656fa83e09e1c8876f495e4bf7e3
xiatianyang <xty697@jisti.com>
2020-10-17 00:01:20 +0800

    save

--- src/views/mod/slideDialog/index.vue
+++ src/views/mod/slideDialog/index.vue
@@ -27,7 +27,7 @@
       <el-drawer
         title="我是标题"
         :visible.sync="drawer"
-        
+        :direction="direction"
         :before-close="handleClose"
       >
         <span>我来啦!</span>
